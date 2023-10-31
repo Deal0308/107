f9824bda43b4b9f6b7444937d0c5f59a000d2c2c
Michael Deal <Deal0308@yahoo.com>
2023-10-30 22:32:38 -0500

    final

--- src/App.js
+++ src/App.js
@@ -1,63 +1,41 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import { React } from 'react';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
 import Catalog from './pages/catalog';
-import Cart from './pages/cart';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 import About from './pages/about';
 import Home from './pages/home';
 import Admin from './pages/admin';
-import { getProducts } from './services/dataservice.js';
-import DataService from './services/dataservice.js';
+import Cart from './pages/cart';
+import GlobalProvider from './store/globalprovider';
 
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import GlobalProvider from '../store/globalprovider';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-// Create a footer Component and render it in the App.js
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
-  const [catalog, setCatalog] = useState([]);
-
-  function loadData() {
-    let data = DataService.getCatalog();
-    console.log(data)
-    setCatalog(data);
-  }
-  
-  useEffect(() => {
-    loadData();
-    // getProducts()
-    //   .then((data) => {
-    //     setCatalog(data);
-    //   })
-    //   .catch((error) => {
-    //     console.error(error);
-    //   });
-  }, []);
-
   return (
     <GlobalProvider>
-    <BrowserRouter>
-      <div className="App">
-        
+      <BrowserRouter>
+        <div className="App">
           <Navbar />
+
           <Routes>
-            <Route path="/" element={<Home catalog={catalog} />} />
-            <Route path="/home" element={<Home catalog={catalog} />} />
-            <Route path="/catalog" element={<Catalog catalog={catalog} />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/admin" element={<Admin />} />
-            <Route path="/cart" element={<h1>Cart</h1>} />
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/home" element={<Home />}></Route>
+            <Route path="/catalog" element={<Catalog />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/admin" element={<Admin />}></Route>
+            <Route path="/cart" element={<Cart />}></Route>
           </Routes>
+
           <Footer />
-       
-      </div>
-    </BrowserRouter>
+        </div>
+      </BrowserRouter>
     </GlobalProvider>
   );
 }
+
 export default App;
